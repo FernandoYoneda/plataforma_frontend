43b8f4ee5df43fca43b6c750124666d5a59065b1
FernandoYoneda <fernandotakeshi1@gmail.com>
2025-08-11 15:14:26 -0300

    Corrigir import duplicados

--- src/OrderForm.js
+++ src/OrderForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import React from 'react'
 
 const OrderForm = () => {
   const [material, setMaterial] = useState("");
